9e3fb4e56e77b2f098cb5ab7c90d6868c8e6c8c0
VladimirE <wortex2014@bk.ru>
2023-02-09 17:24:45 +0400

    dz3

--- HomeWork/DZ_3/dz2.java
+++ HomeWork/DZ_3/dz2.java
@@ -1,5 +1,26 @@
 package HomeWork.DZ_3;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class dz2 {
-    
+    //Пусть дан произвольный список целых чисел, удалить из него четные числа
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(13);
+        arr.add(0);
+        arr.add(5);
+        arr.add(-2);
+        arr.add(20);
+        arr.add(2);
+        arr.add(3);
+        System.out.println(arr);
+        Iterator<Integer> arrayIterator = arr.iterator();
+        while (arrayIterator.hasNext()) {
+            Integer nextArr = arrayIterator.next();
+            if (nextArr % 2 == 0){
+                arrayIterator.remove();
+            }
+        }
+        System.out.println(arr);
+    }
 }
--- HomeWork/DZ_3/dz3.java
+++ HomeWork/DZ_3/dz3.java
@@ -1,5 +1,35 @@
 package HomeWork.DZ_3;
+import java.util.ArrayList;
 
 public class dz3 {
-    
+/* Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее ариф из этого списка. */
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(13);
+        arr.add(0);
+        arr.add(5);
+        arr.add(-2);
+        arr.add(20);
+        arr.add(2);
+        arr.add(3);
+        System.out.println(arr);
+        int min = arr.get(0);
+        int max = arr.get(0);
+        for (Integer i : arr) {
+            if (i < min)
+                min = i;
+            if (i > max)
+                max = i;
+        }
+        System.out.println("минимальное число: " + min);
+        System.out.println("максимальное число: " + max);
+        double sum = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+            sum += arr.get(i);
+        }
+
+         double result = sum / arr.size();
+        System.out.println("Спеднее арифметическое : " + result);
+    }
 }
